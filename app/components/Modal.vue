e30b9c5e2d06d557b31507049bfab212cc890677
abgata20000 <abgata@abgata.jp>
2019-03-30 17:29:53 +0900

    add: サンプル

---
+++ app/components/Modal.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal modal-overlay" @click.self="$emit('close')">
+      <div class="modal__window">
+        <div class="modal__content">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ["~assets/scss/application.scss"],
 
   /*
    ** Plugins to load before mounting the App
